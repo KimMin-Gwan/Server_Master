e1019375dc1ed6e58e5b95832b1d69c09fb26d1d
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-08-10 16:44:07 +0900

    회원가입 중복확인 테스트

--- test_Vespoi/register_tset.py
+++ test_Vespoi/register_tset.py
@@ -4,21 +4,47 @@ import json
 from pymongo import MongoClient
 import certifi
 
+def check_dupe():
+    mongo_connect = "mongodb+srv://admin:r3tgCfkESORu8iO4@cluster0.pmbm4ny.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(mongo_connect, tlsCAFile=certifi.where())
+    db = client.temp
+
+    try: 
+        db_data = list(db.account.find({'phone':{'$regex': '01000000002'}}))
+        if not db_data:
+            print(db_data, 'empty')
+            return False
+        else:
+            print(db_data, 'not empty')
+            return True
+    except:
+        print("none")
+        return False
+
+
 mongo_connect = "mongodb+srv://admin:r3tgCfkESORu8iO4@cluster0.pmbm4ny.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(mongo_connect, tlsCAFile=certifi.where())
 db = client.temp
 data={
-    'name': 'mtest',
-    'phone': '01000000001',
+    'name': 'localdutetest',
+    'phone': '01000000002',
     'pw': 'test1234'
 }
 
-try:
-    db.account.insert_one(data)
-
-except:
-    response = {"message":"register failed"}
+check_reg = check_dupe()
+if(check_reg==True):
+    response = {"message":"register failed phone already exists"}
     print( response)
+   
+else:
+    try:
+        db.account.insert_one(data)
+        response = {"message":"register success"}
+        print( response)
+
+    except:
+        response = {"message":"register failed"}
+        print( response)
+
+
         
-response = {"message":"register success"}
-print( response)
