d0181b34270a24bc6d47f956e1cca51780bccd96
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-08-10 16:50:29 +0900

    회원가입 중복 확인 로컬 서버 테스트

--- python/back_end/register_db.py
+++ python/back_end/register_db.py
@@ -11,23 +11,51 @@ class Register():
         self.phone = phone
         self.pw = pw
 
-    def register(self):
+    def __check_dupe(self):
         mongo_connect = back_end.constant.mongo_key
         client = MongoClient(mongo_connect, tlsCAFile=certifi.where())
         db = client.temp
 
+        try: 
+            db_data = list(db.account.find({'phone':{'$regex': self.phone}}))
+            if not db_data:
+                return False #리스트가 비어있음 ( 데이터 없음 )
+            else:
+                return True #리스트에 정보가 있음
+        except: 
+            return False # 못찾음
+
+    def register(self):
+        #db 연결
+        mongo_connect = back_end.constant.mongo_key
+        client = MongoClient(mongo_connect, tlsCAFile=certifi.where())
+        db = client.temp
+
+        #db에 넘길 데이터
         data={
             'name': self.name,
             'phone': self.phone,
             'pw': self.pw
             }
+        
+        #중복확인
+        check_reg = self.__check_dupe()
 
-        try:
-            db.account.insert_one(data)
-
-        except:
-            response = {"message":"register failed"} #,"status": HTTPStatus.OK
+        if(check_reg==True): #이미 있으면 실패
+            response = {"message":"register failed phone already exists"}
             return response
         
-        response = {"message":"register success"} #,"status": HTTPStatus.OK
+        else:
+            try: #없으면 추가
+                db.account.insert_one(data)
+                response = {"message":"register success"} #,"status": HTTPStatus.OK
+                return response
+
+            except: #없지만 뭔가 문제가 생김 (잘못된? 데이터)
+                response = {"message":"register failed"} #,"status": HTTPStatus.OK
+                return response
+        #몰?루
+        response = {"message":"unknown error"}
         return response
+        
+        
