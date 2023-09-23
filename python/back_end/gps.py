773973887f3a82358d2e4e11d1ea610f2d43c61e
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-09-23 17:27:21 +0900

    gps update

--- python/back_end/gps.py
+++ python/back_end/gps.py
@@ -21,7 +21,7 @@ class GPS():
             "userY" : self.y, #사용자의 현재 Y좌표입니다.
             "sort"  : "distance", #검색 결과 정렬. • score : 랭킹 점수 높은순 (default) • distance : 가까운 거리순 • evcharger : 가까운 거리 내 사용 가능한 EV 충전기 많은순
             "page" : "1", #조회할 목록의 페이지를 지정합니다.
-            "count" : "10", #페이지당 출력되는 개수를 지정합니다.
+            "count" : "1", #페이지당 출력되는 개수를 지정합니다.
             "radius" : "0.1", #km 단위 검색 반경.
             "searchType" : "nearby", # keyword - 일반 키워드 검색 / category - 카테고리검색 (키워드 없음) / nearby - 근처검색( 키워드 없음)
             "lineString" : f'{self.x},{self.y}_{self.x},{self.y}'
@@ -31,11 +31,12 @@ class GPS():
             url = f'https://apis.openapi.sk.com/tmap/poi/findPoiRoute?version={back_end.constant.tmap_version}&appKey={back_end.constant.tmap_key}'
 
             result = requests.post(url, json = payload).json()
-            for i in result["searchPoiInfo"]['pois']['poi']:
-                name_list.append(json.dumps(i['name'],ensure_ascii=False))
-            return name_list
+            # for i in result["searchPoiInfo"]['pois']['poi']:
+            #     name_list.append(json.dumps(i['name'],ensure_ascii=False))
+            # return name_list
+            return result["searchPoiInfo"]['pois']['poi'][0]['name']
         except:
-            return 'default'
+            return "error not found"
 
     def __get_location_biz_name(self): #이게안됨 여러개 말고 단일로 하나만 보내봐야 할듯
         #건물 이름으로 얻은 데이터 분석해서 장소 보내주기 
@@ -55,14 +56,38 @@ class GPS():
 
 
     def gps_analyzer(self):
-        
-        # with open (back_end.constant.AAC_FILE, "r", encoding='cp949') as f:
-        #         saved_data = json.load(f)
-        
-        biz_name_list = self.__get_location_biz_name()
+        #건물 이름으로 얻은 데이터 분석해서 장소 라벨 보내주기 ex)식당, 병원, 약국, 학교
+        headers = back_end.constant.naver_headers
+        #headers = {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
+        url_base="https://openapi.naver.com/v1/search/local.json?query="
+        keyword = self.__get_location_name()
+        url_middle="$&start="
+        number='1'
 
+        url = url_base + keyword + url_middle + number
+        if(keyword == "error not found"):
+            result_json = {'category': 'default'}
+            return result_json
+
+        with open (back_end.constant.AAC_FILE, "r", encoding='cp949') as f:
+                saved_data = json.load(f)
+
+
+
+        result = requests.get(url,headers = back_end.constant.naver_headers).json()
         try:
-            result_json = {'category': biz_name_list}
+            #BdUse = json.dumps(result['items'][0]["category"],indent=4,ensure_ascii=False)
+            BdUse = result['items'][0].pop('category', None)
+            # data_temp = BdUse.split('>')
+            # data= data_temp.split(',')
+            data = BdUse.split('>')
+            # for i in saved_data["AAC"]:
+            #     if(i["name"]==data[-1]):
+            #         result_json = {'category': data[-1]}
+            #         break;
+            #     else:
+            #         result_json = {'category': 'default'}
+            result_json = {'category': data}
             return result_json
         except:
             result_json = {'category': 'default'}
